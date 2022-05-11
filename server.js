c97764cdb6964401ad6dc637ee08230427c65863
itsmebroarif.dev <68406328+itsmebroarif@users.noreply.github.com>
2022-05-11 23:42:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-// Pastikan Kalian Sudah Menjalankan (npm run configure) untuk pertama kali
-// panggil semua data yang sudah di install [mysql, express, ejs]
+// Udah Jalanin  'npm run configure'
+// panggil semua data yang sudah di install [mysql, express, ejs, body-parser, nodemon]
 const express = require('express');
 const mysql = require('mysql');
+
 // use express untuk membuat server backend nya
 const app = express();
 
+// Konfigurasi Index.ejs
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// Konfigurasi MySql
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -16,11 +22,11 @@ const db = mysql.createConnection({
 db.connect((err) => {
  if (err) throw err;
  console.log('MySQL Telah Terhubung');
- 
+
 //  Select nama tabel disini
 const sql = 'SELECT * FROM users';
 
-
+// Tampilkan data yang ada di tabel
 db.query(sql, (err, result) =>{
     // Parse Data
     const users = JSON.parse(JSON.stringify(result));
@@ -31,10 +37,10 @@ db.query(sql, (err, result) =>{
   })
 })
 
+
+// respon aplikasinya ketika server berhasil di jalankan
 app.listen(8000, () => {
     console.log('Selamat Jalan Di Comment.js | Developed by : Arif Alexander');
     console.log('Server Jalan Di port 8000');
     console.log('Jalankan Di http://localhost:8000');
 })
-
-
